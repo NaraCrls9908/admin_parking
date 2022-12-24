b8d6a5bff046cf2af528b95a6e22b8c2d03637b7
NaraCrls9908 <nara.carlos9908@gmail.com>
2022-12-23 17:10:31 -0800

    CRUD Parkings-updated routes

--- API/dist/app.js
+++ API/dist/app.js
@@ -43,40 +43,47 @@ app.get('/', (req, res) => {
 app.get('/parkings', (req, res) => {
     res.send(parkings);
 });
-// Create parkings
-app.post('/add-parking', (req, res) => {
+// Create parking
+app.post('/parkings/add', (req, res) => {
     console.log("body de la req: ", req.body);
     addParking(req.body);
     res.send(parkings);
 });
-app.delete('/delete-parking/:id', (req, res) => {
+// Delete parking
+app.delete('/parkings/delete/:id', (req, res) => {
     let id = parseInt(req.params.id);
     deleteParking(id);
     res.send(parkings);
 });
-app.put('/update-parking/:id', (req, res) => {
+// Update parking
+app.put('/parkings/update/:id', (req, res) => {
     let id = parseInt(req.params.id);
     updateParking(id, req.body);
     console.log(parkings);
     res.send(parkings);
 });
+// Get a specofic parking
 app.get('/parkings/:id', (req, res) => {
     res.send(parkings[req.params.id]);
 });
-// Fuctions
+// Fuctions to manage parkings -> array
+// Delete one element
 function deleteParking(id) {
     console.log("elemento a eliminar-> " + id);
     parkings.splice(id - 1, 1);
 }
+// Ad one element
 function addParking(info) {
     //We asign all the info in each variable
     parkings.push(buildParking(info)); // We add a new parking
 }
+// Delete and add one element in a specific position
 function updateParking(id, info) {
     deleteParking(id);
     let updatedParking = buildParking(info);
     parkings.splice(id - 1, 0, updatedParking);
 }
+// Build a new parking object
 function buildParking(info) {
     let [address, ammenities, score, price, type, images, description] = [info.address,
         info.ammenities,
@@ -96,6 +103,7 @@ function buildParking(info) {
     };
     return new_parkin;
 }
+// Run
 app.listen(port, () => {
     console.log(`Corriendo en el puerto: ${port}`);
 });
