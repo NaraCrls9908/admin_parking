c15964ba0d715a622a604bea336631bb7011a9c8
NaraCrls9908 <nara.carlos9908@gmail.com>
2022-12-24 22:52:43 -0800

    API with filters 99%

--- API/app.ts
+++ API/app.ts
@@ -16,7 +16,7 @@ app.use(cors());
 
 interface Parking {
     address: String,
-    ammenities: String[],
+    amenities: String[],
     score: number,
     price: number,
     type: String,
@@ -27,7 +27,7 @@ interface Parking {
 let parkings: Parking[]= [
     {
         address: 'Street A #35, California, CA,',
-        ammenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Battery-shaped places'],
+        amenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Battery-shaped places'],
         score: 3.8,
         price: 100,
         type: 'Public',
@@ -36,7 +36,7 @@ let parkings: Parking[]= [
     },
     {
         address: 'Street B #27, California, CA',
-        ammenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling'],
+        amenities: ['Surveillance Cam', 'Apartment', 'Parking with ceiling'],
         score: 4.2,
         price: 175,
         type: 'Public',
@@ -45,7 +45,7 @@ let parkings: Parking[]= [
     },
     {
         address: 'Street C #7, California, CA',
-        ammenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling', 'Private Parking Lot'],
+        amenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling', 'Private Parking Lot'],
         score: 4.8,
         price: 200,
         type: 'Private',
@@ -53,6 +53,8 @@ let parkings: Parking[]= [
         description: 'High luxury and security parking. Private parking with surveillance cameras, private parking for your car, aditionali we can wash your car.'
     }
 ]
+
+let parkings_filtered: Parking[];
 
 // Is my API working?
 app.get('/', (req, res)=> {
@@ -67,8 +69,10 @@ app.get('/parkings', (req, res)=> {
 // Create parking
 app.post('/parkings/add', (req, res)=> {
     console.log("body de la req: ", req.body);
-    addParking(req.body);  
-    res.send({title: "Response of parkings/add", status: "Ok", message: "A new parking has been created.", data: parkings[parkings.length-1], type: "POST"})
+    if (addParking(req.body))  
+        res.send({title: "Response of parkings/add", status: "Ok", message: "A new parking has been created.", data: parkings[parkings.length-1], type: "POST"});
+    else 
+        res.send({title: "Response of parkings/add", status: "Failed", message: "The parking couldn't be created.", data: parkings[parkings.length-1], type: "POST"});
 })
 // Delete parking
 app.delete('/parkings/delete/:id', (req, res)=> {
@@ -81,18 +85,51 @@ app.delete('/parkings/delete/:id', (req, res)=> {
 app.put('/parkings/update/:id', (req, res)=> {
     let id = parseInt(req.params.id)
     if(id < parkings.length){
-        updateParking(id, req.body);
-        res.send({title: "Response of parkings/update", status: "Ok", message: "Parking " + id + " has been updated.", data: parkings[id-1], type: "PUT"});
+        if (updateParking(id, req.body))
+            res.send({title: "Response of parkings/update", status: "Ok", message: "Parking " + id + " has been updated.", data: parkings[id-1], type: "PUT"});
     }else 
-        res.send({title: "Response of parkings/update", status: "Not Working", message: "Parking " + id + " has not been found.", data: [], type: "PUT"})
+        res.send({title: "Response of parkings/update", status: "Failed", message: "The parking couldn't be updated.", data: [], type: "PUT"})
 })
-// Get a specofic parking
-app.get('/parkings/:id', (req, res)=> {
+// Get a specific parking
+app.get('/parkings/get-one/:id', (req, res)=> {
     let id = parseInt(req.params.id);
     if(id < parkings.length) 
         res.send({title: "Response of parkings/byId", status: "Ok", message: "Parking " + id + " has been given.", data: parkings[id-1], type: "GET"});
     else
         res.send({title: "Response of parkings/byId", status: "Not Working", message: "Parking " + id + " has not been found.", data: [], type: "GET"});
+})
+// Get parking filtered by Min and Max
+app.get('/parkings/filters', (req, res)=> {
+    parkings_filtered = parkings;
+
+
+    let min_cost = parseFloat(req.query.min_cost.toString());
+    let max_cost = parseFloat(req.query.max_cost.toString());
+    let type = req.query.type.toString();
+    let amenities = req.query.amenities.toString();
+
+    // NaN to null in MIN and MAX cost
+    if(isNaN(min_cost)) min_cost = null;
+    if(isNaN(max_cost)) max_cost = null;
+
+
+
+    console.log("min cost: " + min_cost);
+    console.log("max cost: " + max_cost);
+    console.log("type: " + type);
+    console.log("amenities: " + amenities);
+    //if(min_cost || max_cost) 
+    filterByMinMaxCost(min_cost, max_cost);
+    //if(type) 
+    filterByType(type);
+    //if(amenities) 
+    filterByAmenities(amenities);
+    
+    if(parkings_filtered.length <= 0)
+        res.send({title: "Response of parkings/filtered", status: "Failed", message: "Data not found", data: parkings_filtered, type: "GET"})
+    else
+        res.send({title: "Response of parkings/filtered", status: "Ok", message: "Data of parkings with filters has been given.", data: parkings_filtered, type: "GET"});
+
 })
 
 
@@ -105,18 +142,29 @@ function deleteParking(id:number) {
 // Ad one element
 function addParking(info) {
     //We asign all the info in each variable
-    parkings.push(buildParking(info)); // We add a new parking
+    let new_parking = buildParking(info);
+    if(new_parking) {
+        parkings.push(new_parking); // We add a new parking
+        return true
+    }   
+
+    return false;
 }
 // Delete and add one element in a specific position
 function updateParking(id:number, info) {
     deleteParking(id);
     let updatedParking = buildParking(info)
-    parkings.splice(id-1, 0, updatedParking);
+    if(updatedParking) {
+        parkings.splice(id-1, 0, updatedParking); // We add the updated parking in the same position
+        return true;
+    }
+    
+    return false;
 }
 // Build a new parking object
 function buildParking(info) {
-    let [address, ammenities, score, price, type, images, description] = [info.address, 
-        info.ammenities, 
+    let [address, amenities, score, price, type, images, description] = [info.address, 
+        info.amenities, 
         info.score,
         info.price,
         info.type,
@@ -125,14 +173,76 @@ function buildParking(info) {
 
         let new_parking: Parking = {
         address: address,
-        ammenities: ammenities,
+        amenities: amenities,
         score: score,
         price: price,
         type: type,
         images: images,
         description: description
         } 
-    return new_parking;
+    return validateParking(new_parking) ? new_parking : null;
+}
+
+function validateParking(parking:Parking) {
+    if (parking){
+        if(parking.address != null && 
+                parking.type != null && 
+                    parking.description != null &&
+                        parking.price > 0 &&
+                            (parking.score > 0 && parking.score < 5) &&
+                                parking.images != null) {
+                                    return true            
+        }
+    }
+
+    return false
+}
+
+function filterByMinMaxCost(min_cost:number, max_cost:number) {
+    
+    if (min_cost != null && max_cost != null)
+        return parkings_filtered = parkings.filter(parking => parking.price >= min_cost && parking.price <= max_cost);
+    else if(min_cost != null)
+        return parkings_filtered = parkings.filter(parking => parking.price >= min_cost);
+    else if(max_cost != null)
+        return parkings_filtered = parkings.filter(parking => parking.price <= max_cost);
+    else // Here we have no filters
+        return parkings;
+}
+
+function filterByType(type:string) {
+    if(parkings_filtered != null) {
+        if(type)
+            return parkings_filtered = parkings_filtered.filter(parking => parking.type == type);
+    }else {
+        if(type)
+            return parkings_filtered = parkings.filter(parking => parking.type == type);
+    }
+    return parkings // Here we have no filters;
+}
+
+function filterByAmenities(amenitiesArg:string) {
+    
+    let amenities = amenitiesArg.split(",");
+    amenities = amenities.map(amenitie => amenitie.trim())
+    let parkings_aux: Parking[] = [];
+    if(amenities) {
+        parkings_filtered.forEach(parking => {
+            amenities.forEach(amenitie => {
+                if(parking.amenities.includes(amenitie)) {
+                    parkings_aux.push(parking);
+                }
+            })
+        })
+    
+        return parkings_filtered = parkings_aux.filter((item,index)=>{
+            return parkings_aux.indexOf(item) === index;
+        });
+    }
+
+    return parkings; // Here we have no filters
+
+    
 }
 
 // Run
--- API/dist/app.js
+++ API/dist/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 let parkings = [
     {
         address: 'Street A #35, California, CA,',
-        ammenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Battery-shaped places'],
+        amenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Battery-shaped places'],
         score: 3.8,
         price: 100,
         type: 'Public',
@@ -19,7 +19,7 @@ let parkings = [
     },
     {
         address: 'Street B #27, California, CA',
-        ammenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling'],
+        amenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling'],
         score: 4.2,
         price: 175,
         type: 'Public',
@@ -28,7 +28,7 @@ let parkings = [
     },
     {
         address: 'Street C #7, California, CA',
-        ammenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling', 'Private Parking Lot'],
+        amenities: ['Surveillance Cam', 'Apartment', 'Ground Floor', 'Parking with ceiling', 'Private Parking Lot'],
         score: 4.8,
         price: 200,
         type: 'Private',
@@ -36,6 +36,7 @@ let parkings = [
         description: 'High luxury and security parking. Private parking with surveillance cameras, private parking for your car, aditionali we can wash your car.'
     }
 ];
+let parkings_filtered;
 // Is my API working?
 app.get('/', (req, res) => {
     res.send("Hi! I'm your API and I'm working");
@@ -48,46 +49,61 @@ app.get('/parkings', (req, res) => {
 // Create parking
 app.post('/parkings/add', (req, res) => {
     console.log("body de la req: ", req.body);
-    addParking(req.body);
-<<<<<<< HEAD
-    res.send({ title: "Response of parkings/add", status: "Ok", message: "A new parking has been created.", type: "POST" });
-=======
-    res.send({ title: "Response of parkings/add", status: "Ok", message: "A new parking has been created.", data: parkings[parkings.length - 1], type: "POST" });
->>>>>>> 82dab86494b29667d592fd6a7c11e72636eeebc1
+    if (addParking(req.body))
+        res.send({ title: "Response of parkings/add", status: "Ok", message: "A new parking has been created.", data: parkings[parkings.length - 1], type: "POST" });
+    else
+        res.send({ title: "Response of parkings/add", status: "Failed", message: "The parking couldn't be created.", data: parkings[parkings.length - 1], type: "POST" });
 });
 // Delete parking
 app.delete('/parkings/delete/:id', (req, res) => {
     let id = parseInt(req.params.id);
     deleteParking(id);
-<<<<<<< HEAD
-    res.send({ title: "Response of parkings/delete", status: "Ok", message: "Parking " + id + " has been deleted.", type: "DELETE" });
-=======
     res.send({ title: "Response of parkings/delete", status: "Ok", message: "Parking " + id + " has been deleted.", data: [], type: "DELETE" });
->>>>>>> 82dab86494b29667d592fd6a7c11e72636eeebc1
 });
 // Update parking
 app.put('/parkings/update/:id', (req, res) => {
     let id = parseInt(req.params.id);
-<<<<<<< HEAD
-    updateParking(id, req.body);
-    console.log(parkings);
-    res.send({ title: "Response of parkings/update", status: "Ok", message: "Parking " + id + " has been updated.", type: "PUT" });
-=======
     if (id < parkings.length) {
-        updateParking(id, req.body);
-        res.send({ title: "Response of parkings/update", status: "Ok", message: "Parking " + id + " has been updated.", data: parkings[id - 1], type: "PUT" });
+        if (updateParking(id, req.body))
+            res.send({ title: "Response of parkings/update", status: "Ok", message: "Parking " + id + " has been updated.", data: parkings[id - 1], type: "PUT" });
     }
     else
-        res.send({ title: "Response of parkings/update", status: "Not Working", message: "Parking " + id + " has not been found.", data: [], type: "PUT" });
->>>>>>> 82dab86494b29667d592fd6a7c11e72636eeebc1
+        res.send({ title: "Response of parkings/update", status: "Failed", message: "The parking couldn't be updated.", data: [], type: "PUT" });
 });
-// Get a specofic parking
-app.get('/parkings/:id', (req, res) => {
+// Get a specific parking
+app.get('/parkings/get-one/:id', (req, res) => {
     let id = parseInt(req.params.id);
     if (id < parkings.length)
         res.send({ title: "Response of parkings/byId", status: "Ok", message: "Parking " + id + " has been given.", data: parkings[id - 1], type: "GET" });
     else
         res.send({ title: "Response of parkings/byId", status: "Not Working", message: "Parking " + id + " has not been found.", data: [], type: "GET" });
+});
+// Get parking filtered by Min and Max
+app.get('/parkings/filters', (req, res) => {
+    parkings_filtered = parkings;
+    let min_cost = parseFloat(req.query.min_cost.toString());
+    let max_cost = parseFloat(req.query.max_cost.toString());
+    let type = req.query.type.toString();
+    let amenities = req.query.amenities.toString();
+    // NaN to null in MIN and MAX cost
+    if (isNaN(min_cost))
+        min_cost = null;
+    if (isNaN(max_cost))
+        max_cost = null;
+    console.log("min cost: " + min_cost);
+    console.log("max cost: " + max_cost);
+    console.log("type: " + type);
+    console.log("amenities: " + amenities);
+    //if(min_cost || max_cost) 
+    filterByMinMaxCost(min_cost, max_cost);
+    //if(type) 
+    filterByType(type);
+    //if(amenities) 
+    filterByAmenities(amenities);
+    if (parkings_filtered.length <= 0)
+        res.send({ title: "Response of parkings/filtered", status: "Failed", message: "Data not found", data: parkings_filtered, type: "GET" });
+    else
+        res.send({ title: "Response of parkings/filtered", status: "Ok", message: "Data of parkings with filters has been given.", data: parkings_filtered, type: "GET" });
 });
 // Fuctions to manage parkings -> array
 // Delete one element
@@ -97,18 +113,27 @@ function deleteParking(id) {
 // Ad one element
 function addParking(info) {
     //We asign all the info in each variable
-    parkings.push(buildParking(info)); // We add a new parking
+    let new_parking = buildParking(info);
+    if (new_parking) {
+        parkings.push(new_parking); // We add a new parking
+        return true;
+    }
+    return false;
 }
 // Delete and add one element in a specific position
 function updateParking(id, info) {
     deleteParking(id);
     let updatedParking = buildParking(info);
-    parkings.splice(id - 1, 0, updatedParking);
+    if (updatedParking) {
+        parkings.splice(id - 1, 0, updatedParking); // We add the updated parking in the same position
+        return true;
+    }
+    return false;
 }
 // Build a new parking object
 function buildParking(info) {
-    let [address, ammenities, score, price, type, images, description] = [info.address,
-        info.ammenities,
+    let [address, amenities, score, price, type, images, description] = [info.address,
+        info.amenities,
         info.score,
         info.price,
         info.type,
@@ -116,14 +141,66 @@ function buildParking(info) {
         info.description];
     let new_parking = {
         address: address,
-        ammenities: ammenities,
+        amenities: amenities,
         score: score,
         price: price,
         type: type,
         images: images,
         description: description
     };
-    return new_parking;
+    return validateParking(new_parking) ? new_parking : null;
+}
+function validateParking(parking) {
+    if (parking) {
+        if (parking.address != null &&
+            parking.type != null &&
+            parking.description != null &&
+            parking.price > 0 &&
+            (parking.score > 0 && parking.score < 5) &&
+            parking.images != null) {
+            return true;
+        }
+    }
+    return false;
+}
+function filterByMinMaxCost(min_cost, max_cost) {
+    if (min_cost != null && max_cost != null)
+        return parkings_filtered = parkings.filter(parking => parking.price >= min_cost && parking.price <= max_cost);
+    else if (min_cost != null)
+        return parkings_filtered = parkings.filter(parking => parking.price >= min_cost);
+    else if (max_cost != null)
+        return parkings_filtered = parkings.filter(parking => parking.price <= max_cost);
+    else // Here we have no filters
+        return parkings;
+}
+function filterByType(type) {
+    if (parkings_filtered != null) {
+        if (type)
+            return parkings_filtered = parkings_filtered.filter(parking => parking.type == type);
+    }
+    else {
+        if (type)
+            return parkings_filtered = parkings.filter(parking => parking.type == type);
+    }
+    return parkings; // Here we have no filters;
+}
+function filterByAmenities(amenitiesArg) {
+    let amenities = amenitiesArg.split(",");
+    amenities = amenities.map(amenitie => amenitie.trim());
+    let parkings_aux = [];
+    if (amenities) {
+        parkings_filtered.forEach(parking => {
+            amenities.forEach(amenitie => {
+                if (parking.amenities.includes(amenitie)) {
+                    parkings_aux.push(parking);
+                }
+            });
+        });
+        return parkings_filtered = parkings_aux.filter((item, index) => {
+            return parkings_aux.indexOf(item) === index;
+        });
+    }
+    return parkings; // Here we have no filters
 }
 // Run
 app.listen(port, () => {
